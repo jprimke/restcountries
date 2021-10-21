4f7d0017cd79764a0cae3a9ce5e1bc84d65d5ca4
Jörg PRIMKE <joerg.primke@axa-assistance.de>
2021-10-21 09:19:01 +0200

    Capsule query againts query in the repository

---
+++ src/RestCountries.API/Models/CountryRepositoryOptions.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="D:\PROJEKTE\restcountries\src\RestCountries.API\Models\CountryRepositoryOptions.cs" company="AXA Partners">
+//     Author: Jörg H Primke
+//     Copyright (c) 2021 - AXA Partners. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+
+namespace RestCountries.API.Models;
+
+    public class CountryRepositoryOptions
+    {
+        public string Directory { get; set; } = string.Empty;
+
+        public string FileName { get; set; } = string.Empty;
+    }
